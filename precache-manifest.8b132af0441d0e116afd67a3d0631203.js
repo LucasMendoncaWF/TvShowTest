43ac5b2d3c1057882cdbf653a328afd49b90eba1
Lucas Pedroso <lucasmendoncawf@gmail.com>
2018-10-30 23:57:41 -0300

    upload da build

---
+++ precache-manifest.8b132af0441d0e116afd67a3d0631203.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "238fd6049901a4c150b6f7cf93d423d5",
+    "url": "/static/media/logo-telecine.238fd604.svg"
+  },
+  {
+    "revision": "b5e14b2c2d994878f2a0",
+    "url": "/static/css/main.74db3f26.chunk.css"
+  },
+  {
+    "revision": "65407b2d05b9c494f006",
+    "url": "/static/js/1.65407b2d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6a555c881a12c8f9981eecee28f79696",
+    "url": "/static/media/add-gray-s.6a555c88.svg"
+  },
+  {
+    "revision": "1aa6adbb5be7fe27a14aa1579f34c004",
+    "url": "/static/media/sad-gray-w.1aa6adbb.svg"
+  },
+  {
+    "revision": "b2f181601fc8eab06e37a1609e2f5d0c",
+    "url": "/static/media/rec-gray-s.b2f18160.svg"
+  },
+  {
+    "revision": "2e9ef69e41cb9f50e74ef3b8ffcc447f",
+    "url": "/static/media/share-gray-s.2e9ef69e.svg"
+  },
+  {
+    "revision": "b5e14b2c2d994878f2a0",
+    "url": "/static/js/main.b5e14b2c.chunk.js"
+  },
+  {
+    "revision": "4ee3f7969e6b1c344c4565b4fb6b4b32",
+    "url": "/static/media/play-small-player-w.4ee3f796.svg"
+  },
+  {
+    "revision": "6f47bcfc065790f02ed3cb8b51bef56f",
+    "url": "/static/media/Nunito-Bold.6f47bcfc.ttf"
+  },
+  {
+    "revision": "76e62b379c08d7d94c86ad5301d4abb5",
+    "url": "/static/media/Nunito-Black.76e62b37.ttf"
+  },
+  {
+    "revision": "c41c2502180f63ce383b6e3cc042617a",
+    "url": "/static/media/Nunito-Light.c41c2502.ttf"
+  },
+  {
+    "revision": "26a1ec90be635d027479741f815494cb",
+    "url": "/static/media/Nunito-ExtraLight.26a1ec90.ttf"
+  },
+  {
+    "revision": "65bb0a158ee1967292ee4d11079d45ae",
+    "url": "/static/media/Nunito-Regular.65bb0a15.ttf"
+  },
+  {
+    "revision": "158df717e1d2714d035ba033b0e149c4",
+    "url": "/index.html"
+  }
+];
